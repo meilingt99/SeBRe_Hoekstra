b7cceb9eb1347dc0d68574ad88b28b3fdc05f3e3
Meiling Thompson <meilinglthompson@gmail.com>
2021-04-27 21:05:02 -0400

    Updated custom_dataset_create.py to handle multiple classes

--- custom_dataset_create.py
+++ custom_dataset_create.py
@@ -29,6 +29,8 @@ ROOT_DIR = os.getcwd()
 # in_folder = sys.argv[1]
 # out_folder = sys.argv[2]
 
+mask_colors = [[255,179,190],[186,147,223],[255,224,0],[68,71,149]]
+
 parser = argparse.ArgumentParser(description='custom_dataset_create.py params: input folder and output folder of masks')
 parser.add_argument("--in_folder",required=True,help="mask input directory",action='store',type=str)
 parser.add_argument("--out_folder",required=True,help="mask output directory",action='store',type=str)
@@ -55,30 +57,34 @@ all_masked_sections = natsorted(glob.glob('*'))
 for section_num,section_fold in enumerate(all_masked_sections):
     section_masks_dirname = ROOT_DIR+'/myDATASET/'+args.out_folder+'/section_masks_'+str(section_num) #modified
     if not os.path.exists(section_masks_dirname):
-        print(section_fold)
-        os.chdir(section_fold)
-        masked_image_orig = skimage.io.imread(glob.glob('*')[0])
-        [s1, s2, s3] = np.shape(masked_image_orig)
-        masked_image = skimage.transform.resize(masked_image_orig,(s1,s2,s3))
-        print('debug0')
-        plt.figure(figsize=[20,20])
-#   	 plt.imshow(masked_image)
-        plt.axis('off')
-        plt.title('masked_section_0.png')
-        [size1, size2, size3] = np.shape(masked_image)
-        print('debug1')
-        ### Create empty mask for each region:
-        cortex_mask = np.zeros([size1,size2])
-        print(masked_image.shape)
-    ### Extract all regionwise masks (eg. cortex, hippocampus, cerebellum, etc.)
-        for index,x in np.ndenumerate(masked_image[:,:,0]):
-            if (int(masked_image[index[0],index[1],0]*255) != 0 or int(masked_image[index[0],index[1],1]*255) != 0 or int(masked_image[index[0],index[1],2]*255) != 0):
-                cortex_mask[index[0],index[1]] = 1
-    ### Save binary masks to training or validation folder (set image folder paths accordingly below):
-        section_masks_dirname = ROOT_DIR+'/myDATASET/'+args.out_folder+'/section_masks_'+str(section_num) #modified
-        if not os.path.exists(section_masks_dirname):
-            os.makedirs(section_masks_dirname)
-        print('debug2')
-        skimage.io.imsave(section_masks_dirname+'/section_masks_'+str(section_num)+'_cortex_m1.png', cortex_mask)
-        plt.close("all")
-        os.chdir('..')
+        for i in range(len(mask_colors)):
+            print(section_fold)
+            os.chdir(section_fold)
+            masked_image_orig = skimage.io.imread(glob.glob('*')[0],plugin='pil')
+            [s1, s2, s3] = np.shape(masked_image_orig)
+            masked_image = skimage.transform.resize(masked_image_orig,(s1,s2,s3))
+            print('debug0')
+            plt.figure(figsize=[20,20])
+    #   	 plt.imshow(masked_image)
+            plt.axis('off')
+            plt.title('masked_section_0.png')
+            [size1, size2, size3] = np.shape(masked_image)
+            print('debug1')
+            ### Create empty mask for each region:
+            cortex_mask = np.zeros([size1,size2])
+            print(masked_image.shape)
+        ### Extract all regionwise masks (eg. cortex, hippocampus, cerebellum, etc.)
+            for index,x in np.ndenumerate(masked_image[:,:,0]):
+                if int(masked_image[index[0],index[1],0]*255) != 0:
+                    print(int(masked_image[index[0],index[1],0]*255),int(masked_image[index[0],index[1],1]*255),int(masked_image[index[0],index[1],2]*255))
+                    print(masked_image[index[0],index[1],0],masked_image[index[0],index[1],1],masked_image[index[0],index[1],2])
+                if (abs(mask_colors[i][0] - int(masked_image[index[0],index[1],0]*255)) <= 5) and (abs(mask_colors[i][1] - int(masked_image[index[0],index[1],1]*255)) <= 5) and (abs(mask_colors[i][2] - int(masked_image[index[0],index[1],2]*255)) <= 5):
+                    cortex_mask[index[0],index[1]] = 1
+        ### Save binary masks to training or validation folder (set image folder paths accordingly below):
+            section_masks_dirname = ROOT_DIR+'/myDATASET/'+args.out_folder+'/section_masks_'+str(section_num) #modified
+            if not os.path.exists(section_masks_dirname):
+                os.makedirs(section_masks_dirname)
+            print('debug2')
+            skimage.io.imsave(section_masks_dirname+'/section_masks_'+str(section_num)+ '_' + str(i+1) + '.png', cortex_mask)
+            plt.close("all")
+            os.chdir('..')
